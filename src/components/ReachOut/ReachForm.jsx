c607563432f59e6951802f66149af74c1e2310b5
cheshta <96103184+cheshta-dua@users.noreply.github.com>
2022-12-19 03:46:56 +0000

    centered form

--- src/components/ReachOut/ReachForm.jsx
+++ src/components/ReachOut/ReachForm.jsx
@@ -5,7 +5,9 @@ const ReachForm = () => {
             <h1>Reach Out</h1>
             <h3>Have a feedback for us? Don’t hesitate to drop it down & we’ll be in touch!</h3>
             {/* Google Form */}
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScJu385FEBf50-vQtci2ZvMMVmw13lWi8iKQUVg05bX-OsXkA/viewform?embedded=true" width="640" height="591" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            <div class="form">
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScJu385FEBf50-vQtci2ZvMMVmw13lWi8iKQUVg05bX-OsXkA/viewform?embedded=true" width="60%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            </div>
         </div>
     )
 }
