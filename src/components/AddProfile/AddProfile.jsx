bda8e372003a6843bcb3ff6ed7f2c8afcec5b8d6
cheshta <96103184+cheshta-dua@users.noreply.github.com>
2022-12-26 21:29:21 +0530

    added css to add_profile page

--- src/components/AddProfile/AddProfile.jsx
+++ src/components/AddProfile/AddProfile.jsx
@@ -1,45 +1,52 @@
 import { useNavigate } from "react-router-dom";
 import "./index.css";
 const AddProfile = () => {
-    const navigate=useNavigate();
+    const navigate = useNavigate();
     var userData = localStorage.getItem('User') || "[]";
     const user = JSON.parse(userData);
-    const NextbtnHandle=()=>{
+    const NextbtnHandle = () => {
         navigate('/upload-resume-page');
     }
     return (
 
         <div className="addP-container">
             <div>
-                <h2 className="">Welcome, {user.name}</h2>
-                <p>Complete your profile to start networking</p>
+                <h2 className="">Welcome, {user.name}!</h2>
+                <p class="subheading">Complete your profile to start networking</p>
             </div>
             <div>
-                <div>
-                    <form>
-                        <label>
-                            Course:
+                <form class="profileForm">
+                    <div class="details1">
+                        <div>
+                            <label>
+                                Course:
+                            </label>
                             <select>
                                 <option>BCA</option>
                                 <option>BBA</option>
                             </select>
-                        </label>
-                        <label>
-                            Batch:
+                        </div>
+                        <div>
+                            <label>
+                                Batch:
+                            </label>
                             <select>
-                                <option>2019-2022</option>
-                                <option>2018-2021</option>
                                 <option>2017-2020</option>
+                                <option>2016-2019</option>
+                                <option>2015-2018</option>
                             </select>
-                        </label>
-                        <label >
+                        </div>
+                    </div>
+                    <div class="details2">
+                        <label>
                             Headline of your profile:
-                            <input type="text"/>
                         </label>
+                        <input type="text" placeholder="where do you work?" />
+                    </div>
+                    <div>
                         <button onClick={NextbtnHandle}>Next</button>
-                    </form>
-                </div>
-                <div></div>
+                    </div>
+                </form>
             </div>
 
         </div>
