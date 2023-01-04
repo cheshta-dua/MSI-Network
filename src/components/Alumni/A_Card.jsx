59c976f9f05348b3d2abbfe80fc2350acac9d2ed
Cheshta <cheshtadua10@gmail.com>
2023-01-04 23:21:13 +0530

    minor layout changes

--- src/components/Alumni/A_Card.jsx
+++ src/components/Alumni/A_Card.jsx
@@ -3,7 +3,7 @@ const A_Card = ({ para }) => {
     return <>
         <div className="cardContainer">
             <div className="userImg"></div>
-            <div className="userDetail">
+            <div className="userDetailHome">
                 <div className="userName">{para.name}</div>
                 <div className="userCompany">{para.company}</div>
             </div>
