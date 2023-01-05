9897a48e307e5929fc04dd13fe40017d1d3ddc7f
Cheshta <cheshtadua10@gmail.com>
2023-01-05 19:13:37 +0530

    enhanced home page

--- src/components/ReachOut/ReachForm.jsx
+++ src/components/ReachOut/ReachForm.jsx
@@ -4,7 +4,6 @@ const ReachForm = () => {
         <div id="ReachOut">
             <h1>Reach Out</h1>
             <h3>Have a feedback for us? Don’t hesitate to drop it down & we’ll be in touch!</h3>
-            {/* Google Form */}
             <div class="form">
                 <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScJu385FEBf50-vQtci2ZvMMVmw13lWi8iKQUVg05bX-OsXkA/viewform?embedded=true" width="60%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
             </div>
--- src/components/college/CollegeComp.jsx
+++ src/components/college/CollegeComp.jsx
@@ -12,9 +12,6 @@ const CollegeComp = () => {
                         One of the most preferred institutes by the students in GGSIPU, the Institute boasts of a sprawling lush green campus of 8 acres, well equipped library, qualified and experienced staff, state of art infrastructure and robust teaching pedagogy. With highly sought after courses like BBA, B.Com, BCA and B.Ed, the Institute aims to empower minds that are able to conceive creative ideas and identify innovative solutions to the problems. In terms of placement, MSI boasts of not only a steady stream of campus recruiters but we have also assurances for future tie-ups and recruitment opportunities for our students. In the year 2018, 573 campus placements and in the year 2017, 385 under-graduate BBA and BCA students have been placed in reputed organizations. To ensure that our students get placed well in big organizations, placement team and faculty members organize various pre-placement activities like Group discussions, Mock interviews etc.
                     </p>
                     <p>
-                        MSI has a well stacked Central Library containing more than 27892 volumes, 85 national and international journals (printed), periodicals, magazines and newspapers. On-line journals totaled 3863 & E-Book are 1257. MSI has an Intellectual capital comprising of 74 faculty members with 40 Ph.D. and 21 M.Phil., 10 M.Tech., 32 UGC NET, 07 Ph.D. (Pursuing) dedicated to facilitate acquisition of conceptual understanding, skills and requisite behavioral qualities to groom students into tomorrow’s Global Professionals.
-                    </p>
-                    <p>
                         Visit the official website <a href="https://www.msijanakpuri.com/" target="_blank">by clicking here. </a>
                     </p>
                 </div>
