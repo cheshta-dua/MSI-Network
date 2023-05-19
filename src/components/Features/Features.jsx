69ec30a55fbec07712634e059ad81fc511bbe624
Cheshta <cheshtadua10@gmail.com>
2023-05-19 20:53:10 +0530

    new section added to homepage

---
+++ src/components/Features/Features.jsx
@@ -0,0 +1,103 @@
+// this is the timeline thingy. not to be deleted rn. might be useful somewhere else later. 
+
+// import { Chrono } from "react-chrono";
+// const Features = () => {
+//     return (
+//         <>
+//             <div className="timeline">
+//                 <h1>What's for you?</h1>
+//                 <div style={{ width: '800px', height: '1000px' }}>
+//                     <Chrono
+//                         mode="VERTICAL_ALTERNATING"
+//                         hideControls
+//                         disableClickOnCircle
+//                         borderLessCards
+//                         theme={{
+//                             primary: '#d4d4d4',
+//                             secondary: '#d4d4d4',
+//                             cardBgColor: '#f1f1f1',
+//                             cardDetailedTextColor: 'yellow',
+//                             cardSubtitleColor: 'var(--blue)',
+//                         }}>
+//                         <div>
+//                             <img src="<url to  a nice image" />
+//                             <p>Upload your professional details</p>
+//                         </div>
+//                     </Chrono>
+//                 </div>
+//             </div>
+//         </>
+//     )
+// }
+// export default Features;
+
+
+import "./Features.css";
+const Features = () => {
+    return (
+        <>
+            <div className="features">
+                <h1>Why join us?</h1>
+                <div className="feature-card">
+                    <div className="feature-card-txt">
+                        <h3>
+                            Lifelong Connections
+                        </h3>
+                        <p>
+                            Build valuable relationships with fellow alumni and faculty members
+                        </p>
+                    </div>
+                    <div className="feature-card-img">
+                        <img src="images/connection-illustration.png" alt="connection-illustration" />
+                    </div>
+
+                </div>
+
+                <div className="feature-card">
+                    <div className="feature-card-img">
+                        <img src="images/career-img.png" alt="illustration" />
+                    </div>
+                    <div className="feature-card-txt">
+                        <h3>
+                            Exclusive Resources
+                        </h3>
+                        <p>
+                            Access the exclusive job opportunities, mentorship programs and events
+                        </p>
+                    </div>
+                </div>
+
+                <div className="feature-card">
+                    <div className="feature-card-txt">
+                        <h3>
+                            Give Back
+                        </h3>
+                        <p>
+                            Contribute to the community through mentoring, donating and supporting current students
+                        </p>
+                    </div>
+                    <div className="feature-card-img">
+                        <img src="images/help.png" alt="illustration" />
+                    </div>
+
+                </div>
+
+                <div className="feature-card">
+                    <div className="feature-card-img">
+                        <img src="images/updated.png" alt="illustration" />
+                    </div>
+                    <div className="feature-card-txt">
+                        <h3>
+                            Stay Updated
+                        </h3>
+                        <p>
+                            Keep up with the current news, events and societies of college
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+export default Features;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,13 +2,15 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import ReachOut from "../../components/ReachOut/ReachForm";
 import Footer from "../../components/Footer/Footer";
-import CollegeComp from "../../components/college/CollegeComp";
+import Features from "../../components/Features/Features";
+// import CollegeComp from "../../components/college/CollegeComp";
 import InstaPosts from "../../components/InstaPosts/Insta";
 const Home = () => {
     return <>
-        
+
         <Header />
-        <CollegeComp />
+        <Features />
+        {/* <CollegeComp /> */}
         <InstaPosts />
         <ReachOut />
         <Footer />
