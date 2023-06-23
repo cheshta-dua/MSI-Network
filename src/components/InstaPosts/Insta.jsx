1977266c44fb3ef9dcdbfa32480f62694a035e22
Cheshta <cheshtadua10@gmail.com>
2023-06-23 14:24:15 +0530

    Insta posts converted to ss

--- src/components/InstaPosts/Insta.jsx
+++ src/components/InstaPosts/Insta.jsx
@@ -11,10 +11,11 @@ const Insta = () => {
                     </h1>
                 </div>
                 <div className="posts">
-                    <InstagramEmbed url="https://www.instagram.com/p/CkC_NMpBg1o/?utm_source=ig_web_copy_link" width={328} captioned className="Post" />
+                    {/* <InstagramEmbed url="https://www.instagram.com/p/CkC_NMpBg1o/?utm_source=ig_web_copy_link" width={328} captioned className="Post" />
                     <InstagramEmbed url="https://www.instagram.com/p/CkC42kABWoF/?utm_source=ig_web_copy_link" width={328} captioned className="Post" />
                     <InstagramEmbed url="https://www.instagram.com/reel/CkC4R7MBFAL/?utm_source=ig_web_copy_link" width={328} captioned className="Post" />
-                    <InstagramEmbed url="https://www.instagram.com/p/Cj-ZVMdPGH4/?utm_source=ig_web_copy_link" width={328} captioned className="Post" />
+                    <InstagramEmbed url="https://www.instagram.com/p/Cj-ZVMdPGH4/?utm_source=ig_web_copy_link" width={328} captioned className="Post" /> */}
+                    <img src="images/Insta-Posts.jpg" width={"98%"}></img>
                 </div>
             </div>
         </Slide>
